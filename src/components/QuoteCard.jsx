788e861b1c49ad2f4f054a11ea5ca241c6ab1235
Olivia Burgos <oliviaburgosblanco@gmail.com>
2023-10-08 22:11:42 -0500

    second_commit

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -9,16 +9,23 @@ const QuoteCard = ({quote, handleChangeQuote}) => {
   return (
     <section className="quoteCard">
         <article className="quoteCard__container">
-<h1 className="quoteCard__title">Infogalax</h1>
-
+ 
+<img src="/title.svg" alt="" />
+ 
 <p className="quoteCard__phrase">{quote.phrase}</p>
+ 
 
-<button className="quoteCard__btn" onClick={handleChangeQuote}>Change</button>
+<button className="quoteCard__btn" onClick={handleChangeQuote}><i className='bx bx-shuffle'></i></button>
         </article>
+     <div className="quoteCard__sphere">
+     <img src="/sphere.png" alt="" />
         <footer className="quoteCard__footer">
+        
             Author: {quote.author}
         </footer>
     
+     </div>
+     
 
     </section>
   )
